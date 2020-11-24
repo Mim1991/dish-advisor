c919f782d446d61bff3189fd7e1adcf83cf26bc2
Jess Silva Carvalho <jc.carvalho@outlook.com>
2020-11-24 15:58:40 +0000

    Added review button to restaurant show and fixed review controller action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,20 +1,28 @@
 class ReviewsController < ApplicationController
+  before_action :find_dish, only: [:new, :create]
   def new
     @review = Review.new
   end
 
   def create
-    @dish = Dish.find(params[:id])
     @review = Review.new(review_params)
+    @review.dish = @dish
+    @review.user = current_user
     if @review.save
-      redirect_to dish_path(@dish)
+      redirect_to restaurant_path(@dish.restaurant)
     else
-      render new
+      render "reviews/new"
+    end
   end
 
   private
+
+  def find_dish
+    @dish = Dish.find(params[:dish_id])
+  end
 
   def review_params
     params.require(:review).permit(:title, :content, :rating, :visit_date)
   end
 end
+
