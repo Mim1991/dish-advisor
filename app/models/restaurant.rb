8d8d2db0f646a01bb4163b1aeb2090d0cae7eb6e
Chiara Agazzi <gyggeaka@hotmail.com>
2020-11-25 12:53:12 +0100

    search restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,11 @@
 class Restaurant < ApplicationRecord
   has_many :dishes
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_cuisine_address,
+  against: [:name, :cuisine, :address],
+  using: {
+    tsearch: {
+      prefix: true } }
+  pg_search_scope :search_by_name_description_allergen, associated_against: [:name, :description, :allergen], using: { tsearch: { prefix: true } }
 end
