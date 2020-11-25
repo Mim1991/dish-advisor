3d2c3bb9d48defb1ad929c2b405d5ff57cb921f2
Chiara Agazzi <gyggeaka@hotmail.com>
2020-11-25 13:17:01 +0100

    dish search

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,4 +2,13 @@ class Dish < ApplicationRecord
   belongs_to :restaurant
   has_many :reviews
   has_many :favourites
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_description_allergen,
+  against:
+  [:name, :description, :allergen],
+  using: {
+    tsearch:{
+      prefix: true }
+  }
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,5 +7,5 @@ class Restaurant < ApplicationRecord
   using: {
     tsearch: {
       prefix: true } }
-  pg_search_scope :search_by_name_description_allergen, associated_against: [:name, :description, :allergen], using: { tsearch: { prefix: true } }
+
 end
