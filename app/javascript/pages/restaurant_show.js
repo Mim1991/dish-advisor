65d138b4385b094b14a335318376a2a9f9fd4de1
meehir patel <meehir.patel12@gmail.com>
2020-11-25 16:37:55 +0000

    popup

--- app/javascript/pages/restaurant_show.js
+++ app/javascript/pages/restaurant_show.js
@@ -1,11 +1,20 @@
 
 const initModal  = () => {
   const dishCard = document.querySelectorAll(".dish-card")
-  const modalBox = document.querySelector(".modal-box")
+
 
   dishCard.forEach ((dish) => {
     dish.addEventListener("click", (event) => {
+      const modalBox = dish.querySelector(".modal-box-total")
       modalBox.classList.toggle("display-block")
+      const barChart = dish.querySelectorAll(".number-stars")
+      console.log(barChart)
+      barChart.forEach((bar) => {
+        const barColour = bar.querySelector(".bar-colour")
+        const starsRating = bar.querySelector(".rating-count-star").innerHTML
+        const starsRatingInt = parseInt(starsRating)
+        barColour.style.width = 14 * starsRatingInt + "%"
+      })
     })
   })
 }
