d8182113994d871a62385d0ef0dbfb2cc12cd587
Chiara Agazzi <gyggeaka@hotmail.com>
2020-11-26 18:51:44 +0100

    map2

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,7 +12,9 @@ class RestaurantsController < ApplicationController
       @markers = @restaurants.geocoded.map do |restaurant|
       {
         lat: restaurant.latitude,
-        lng: restaurant.longitude
+        lng: restaurant.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { restaurant: restaurant }),
+        image_url: helpers.asset_url('372_36_1440760950.jpg')
       }
       end
     end
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -12,7 +12,7 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10';
+      style: 'mapbox://styles/mapbox/streets-v10'
     });
 
     const markers = JSON.parse(mapElement.dataset.markers);
@@ -23,6 +23,26 @@ const initMapbox = () => {
       });
   fitMapToMarkers(map, markers);
   };
+};
+
+const addMarkersToMap = (map, markers) => {
+  markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+
+    // Create a HTML element for your custom marker
+      const element = document.createElement('div');
+      element.className = 'marker';
+      element.style.backgroundImage = `url('${marker.image_url}')`;
+      element.style.backgroundSize = 'contain';
+      element.style.width = '25px';
+      element.style.height = '25px';
+
+    new mapboxgl.Marker(element)
+      .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup) // add this
+      .addTo(map);
+  });
+
 };
 
 export { initMapbox };
