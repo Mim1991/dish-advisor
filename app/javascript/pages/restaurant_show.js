2a7c04dfeb7fd537dc92c366370aab526d40dbeb
meehir patel <meehir.patel12@gmail.com>
2020-11-26 19:55:55 +0000

    updated

--- app/javascript/pages/restaurant_show.js
+++ app/javascript/pages/restaurant_show.js
@@ -6,8 +6,17 @@ const initModal  = () => {
     dish.addEventListener("click", (event) => {
       const modalBox = dish.querySelector(".modal-box-total")
       const screenOverlay = dish.querySelector(".screen-overlay")
+      const closeModal = dish.querySelector(".close-modal")
+      // opens the modal
       modalBox.classList.toggle("display-block")
       screenOverlay.classList.toggle("display-block")
+
+      // const modalBoxOpen = dish.querySelector(".modal-box-total")
+      // const screenOverlayOpen = dish.querySelector(".screen-overlay")
+      // closeModal.addEventListener("click", (event) => {
+      //     modalBoxOpen.classList.remove("display-block")
+      //     screenOverlay.classList.remove("display-block")
+      // })
 
       const barChart = dish.querySelectorAll(".number-stars")
       const reviewCount = dish.querySelector("#review-count-select").innerHTML
@@ -19,6 +28,16 @@ const initModal  = () => {
         const starsRatingInt = parseFloat(starsRating)
         barColour.style.width = 100 * (starsRatingInt / reviewCountInt) + "%"
       })
+
+      const favouriteButton = dish.querySelector(".fave-button")
+      const linkSelect = dish.querySelector(".link-select")
+      favouriteButton.addEventListener("click", (event) => {
+        console.log(linkSelect.innerHTML)
+        linkSelect.innerHTML = "ADDED TO FAVOURITES"
+        linkSelect.classList.remove("link-select")
+        linkSelect.classList.add("black")
+      })
+
     })
   })
 }
