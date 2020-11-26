99b516b90fa446711e48f3700f77504e3be71b28
Chiara Agazzi <gyggeaka@hotmail.com>
2020-11-26 18:02:17 +0100

    search and map

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,7 +9,12 @@ class RestaurantsController < ApplicationController
       @restaurants = Restaurant.search_by_name_cuisine_address(params[:query])
     else
       @restaurants = Restaurant.all
+      @markers = @restaurants.geocoded.map do |restaurant|
+      {
+        lat: restaurant.latitude,
+        lng: restaurant.longitude
+      }
+      end
     end
   end
-
 end
