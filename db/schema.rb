63e140514d36b31b20c9e7ed2d8ea1f9c97d1464
Jess Silva Carvalho <jc.carvalho@outlook.com>
2020-11-27 11:16:22 +0000

    Add dotenv - Protect my secret data in .env file

--- db/schema.rb
+++ db/schema.rb
@@ -9,8 +9,8 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 2020_11_26_145154) do
 
+ActiveRecord::Schema.define(version: 2020_11_26_164129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
