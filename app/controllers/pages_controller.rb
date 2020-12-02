b69f9a2459577ea21bcd59090552f1242c3f7019
Jess Silva Carvalho <jc.carvalho@outlook.com>
2020-12-02 10:31:32 +0000

    heroku push

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -29,9 +29,7 @@ class PagesController < ApplicationController
     @markers = @restaurants.geocoded.map do |restaurant|
       {
         lat: restaurant.latitude,
-        lng: restaurant.longitude,
-        infoWindow: render_to_string(partial: "info_window", locals: { restaurant: restaurant }),
-        image_url: helpers.asset_url('372_36_1440760950.jpg')
+        lng: restaurant.longitude
       }
     end
   end
