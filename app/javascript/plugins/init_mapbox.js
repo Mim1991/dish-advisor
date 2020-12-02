f880a887c0cb009dda516b9a1e688e6cd8eaf324
Chiara Agazzi <gyggeaka@hotmail.com>
2020-12-02 20:10:00 +0100

    pinok

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -28,8 +28,8 @@ const initMapbox = () => {
         element.className = 'marker';
         element.style.backgroundImage = `url('${marker.image_url}')`;
         element.style.backgroundSize = 'contain';
-        element.style.width = '35px';
-        element.style.height = '35px';
+        element.style.width = '22px';
+        element.style.height = '40px';
         new mapboxgl.Marker(element)
         .setLngLat([ marker.lng, marker.lat ])
         .setPopup(popup) // add this
@@ -49,8 +49,8 @@ const initMapbox = () => {
       element.className = 'marker';
       element.style.backgroundImage = `url('${marker.image_url}')`;
       element.style.backgroundSize = 'contain';
-      element.style.width = '35px';
-      element.style.height = '35px';
+      element.style.width = '22px';
+      element.style.height = '40px';
       new mapboxgl.Marker(element)
       .setLngLat([ marker.lng, marker.lat ])
       .setPopup(popup) // add this
